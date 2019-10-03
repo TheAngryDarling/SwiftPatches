b909409925e80ca50eedbfe24b9d09280767e8e6
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-10-03 12:17:35 -0400

    Added new String.Encoding.init(charSet:)

---
+++ Sources/SwiftPatches/extensions/String.Encoding+SwiftPatches.swift
@@ -0,0 +1,32 @@
+//
+//  String.Encoding+SwiftPatches.swift
+//  SwiftPatches
+//
+//  Created by Tyler Anger on 2019-10-03.
+//
+
+import Foundation
+import CoreFoundation
+
+fileprivate extension String {
+    /// CFString version of the current string
+    var cfString: CFString {
+        let chars = Array(self.utf16)
+        let cfStr = CFStringCreateWithCharacters(nil, chars, self.utf16.count)
+        let str = CFStringCreateCopy(nil, cfStr)!
+        return str
+    }
+}
+public extension String.Encoding {
+    // Map selected IANA character set names to encodings, see
+    // https://www.iana.org/assignments/character-sets/character-sets.xhtml
+    init?(charSet: String) {
+        // https://stackoverflow.com/questions/44730379/how-can-i-convert-a-string-such-as-iso-8859-1-to-its-string-encoding-counte
+        
+        let cfe = CFStringConvertIANACharSetNameToEncoding(charSet.cfString)
+        if cfe == kCFStringEncodingInvalidId { return nil }
+        let se = CFStringConvertEncodingToNSStringEncoding(cfe)
+        
+        self.init(rawValue: se)
+    }
+}
