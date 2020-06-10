9d7cdbb20611d047263c1cf5ff28a8afc4c5bfa3
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2020-06-10 13:47:57 -0400

    Added SwiftPatches.swift to include and export sub modules therefore not breaking any projects that previously used this module

---
+++ Sources/SwiftPatches/SwiftPatches.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftPatches.swift
+//  SwiftPatches
+//
+//  Created by Tyler Anger on 2020-06-10.
+//
+
+import Foundation
+@_exported import CorePatches
+@_exported import SequencePatches
+@_exported import NumericPatches
