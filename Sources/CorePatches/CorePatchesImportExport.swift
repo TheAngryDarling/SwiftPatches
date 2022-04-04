a2977275adf7d8742764a2630e5ca5d0f52ce982
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2022-04-04 12:00:56 -0400

    Added new Import/Export for ResultPatches, HashablePatches, StringPatches due the objects in them used to be in CorePatches and this helps keep backwards compatibility

---
+++ Sources/CorePatches/CorePatchesImportExport.swift
@@ -0,0 +1,12 @@
+//
+//  CorePatchesImportExport.swift
+//  CorePatches
+//
+//  Created by Tyler Anger on 2021-03-23.
+//
+
+import Foundation
+
+@_exported import ResultPatches
+@_exported import HashablePatches
+@_exported import StringPatches
