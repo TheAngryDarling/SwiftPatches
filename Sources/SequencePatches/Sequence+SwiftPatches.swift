9ab74a8095be5d7b7feb569be7f4f75ed08e198b
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2022-04-04 20:38:47 -0400

    Removed contains(where:) from Sequence extension due to the fact that it already existed and this was causing ambiguity errors

--- Sources/SequencePatches/Sequence+SwiftPatches.swift
+++ Sources/SequencePatches/Sequence+SwiftPatches.swift
@@ -43,50 +43,6 @@ public extension Sequence {
             }
             return rtn
         }
-    
-        /// Returns a Boolean value indicating whether the sequence contains an
-        /// element that satisfies the given predicate.
-        ///
-        /// You can use the predicate to check for an element of a type that
-        /// doesn't conform to the `Equatable` protocol, such as the
-        /// `HTTPResponse` enumeration in this example.
-        ///
-        ///     enum HTTPResponse {
-        ///         case ok
-        ///         case error(Int)
-        ///     }
-        ///
-        ///     let lastThreeResponses: [HTTPResponse] = [.ok, .ok, .error(404)]
-        ///     let hadError = lastThreeResponses.contains { element in
-        ///         if case .error = element {
-        ///             return true
-        ///         } else {
-        ///             return false
-        ///         }
-        ///     }
-        ///     // 'hadError' == true
-        ///
-        /// Alternatively, a predicate can be satisfied by a range of `Equatable`
-        /// elements or a general condition. This example shows how you can check an
-        /// array for an expense greater than $100.
-        ///
-        ///     let expenses = [21.37, 55.21, 9.32, 10.18, 388.77, 11.41]
-        ///     let hasBigPurchase = expenses.contains { $0 > 100 }
-        ///     // 'hasBigPurchase' == true
-        ///
-        /// - Parameter predicate: A closure that takes an element of the sequence
-        ///   as its argument and returns a Boolean value that indicates whether
-        ///   the passed element represents a match.
-        /// - Returns: `true` if the sequence contains an element that satisfies
-        ///   `predicate`; otherwise, `false`.
-        ///
-        /// - Complexity: O(*n*), where *n* is the length of the sequence.
-        func contains(where predicate: (Element) throws -> Bool) rethrows -> Bool {
-            for element in self {
-                if try predicate(element) { return true }
-            }
-            return false
-        }
     #endif
     
     #if !swift(>=4.1.4)
